02fd50e5443d870ce976967922849d0ac378a948
CHIBOUB Mohamed <mohamed.chiboub@etu.univ-st-etienne.fr>
2025-12-02 15:50:19 +0100

    test

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -15,4 +15,37 @@ final class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
         ]);
     }
+
+ #[Route('/CV', name: 'app_cv')]
+    public function CV(): Response
+    {
+        return $this->render('accueil/CV.html.twig', [
+            'controller_name' => 'CV',
+        ]);
+    }
+
+ #[Route('/Login', name: 'app_Login')]
+    public function Login(): Response
+    {
+        return $this->render('accueil/Login.html.twig', [
+            'controller_name' => 'Login',
+        ]);
+    }
+
+#[Route('/Loisir', name: 'app_Loisir')]
+    public function Loisir(): Response
+    {
+        return $this->render('accueil/Loisir.html.twig', [
+            'controller_name' => 'Loisir',
+        ]);
+    }
+
+#[Route('/Mes_info', name: 'app_Mes_info')]
+    public function Mes_info(): Response
+    {
+        return $this->render('accueil/Mes_info.html.twig', [
+            'controller_name' => 'Mes_info',
+        ]);
+    }
+    
 }
