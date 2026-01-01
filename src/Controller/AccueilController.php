012ba3d6b323610749be83518b5d67c53c201355
CHIBOUB Mohamed <mohamed.chiboub@etu.univ-st-etienne.fr>
2026-01-01 16:42:20 +0100

    mode anglais en cours

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,41 +11,54 @@ final class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+        return $this->render('accueil/index.html.twig');
+    }
+    #[Route('/{_locale<fr|en>}', name: 'homepage')]
+    public function indgex(): Response
+    {
+    return $this->render('accueil/index.html.twig');
     }
 
- #[Route('/CV', name: 'app_cv')]
-    public function CV(): Response
+
+
+    #[Route('/CV', name: 'app_cv')]
+    public function cv(): Response
     {
-        return $this->render('accueil/CV.html.twig', [
-            'controller_name' => 'CV',
-        ]);
+        return $this->render('accueil/cv.html.twig');
     }
 
- #[Route('/Login', name: 'app_Login')]
-    public function Login(): Response
+
+
+    #[Route('/Login', name: 'app_login')]
+    public function login(): Response
     {
-        return $this->render('accueil/Login.html.twig', [
-            'controller_name' => 'Login',
-        ]);
+        return $this->render('accueil/login.html.twig');
     }
 
-#[Route('/Loisir', name: 'app_Loisir')]
-    public function Loisir(): Response
+
+
+    #[Route('/Loisir', name: 'app_loisir')]
+    public function loisir(): Response
     {
-        return $this->render('accueil/Loisir.html.twig', [
-            'controller_name' => 'Loisir',
-        ]);
+        return $this->render('accueil/loisir.html.twig');
     }
 
-#[Route('/Mes_info', name: 'app_Mes_info')]
-    public function Mes_info(): Response
+
+
+    #[Route('/Contact', name: 'app_contact')]
+    public function contact(): Response
     {
-        return $this->render('accueil/Mes_info.html.twig', [
-            'controller_name' => 'Mes_info',
-        ]);
+        return $this->render('accueil/contacte.html.twig');
     }
+
+
     
+    #[Route('/E-Portfolio', name: 'app_e_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('accueil/e-portfolio.html.twig');
+    }
 }
+
+
+
