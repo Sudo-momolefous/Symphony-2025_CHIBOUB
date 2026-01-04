dee8cc6664d9dc65c90afe7792563af697b4dc2f
CHIBOUB Mohamed <mohamed.chiboub@etu.univ-st-etienne.fr>
2026-01-04 13:41:01 +0100

    trie des pages de langues

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,36 +11,36 @@ final class AccueilController extends AbstractController
     #[Route('/{_locale}', name: 'app_accueil', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function index(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/index-en.html.twig' : 'accueil/index.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/index.html.twig' : 'accueil/fr/index.html.twig');
     }
 
     #[Route('/{_locale}/CV', name: 'app_cv', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function cv(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/cv-en.html.twig' : 'accueil/cv.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/cv.html.twig' : 'accueil/fr/cv.html.twig');
     }
 
     #[Route('/{_locale}/Login', name: 'app_login', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function login(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/login-en.html.twig' : 'accueil/login.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/login.html.twig' : 'accueil/fr/login.html.twig');
     }
 
     #[Route('/{_locale}/Loisir', name: 'app_loisir', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function loisir(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/loisir-en.html.twig' : 'accueil/loisir.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/loisir.html.twig' : 'accueil/fr/loisir.html.twig');
     }
 
     #[Route('/{_locale}/Contact', name: 'app_contact', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function contact(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/contacte-en.html.twig' : 'accueil/contacte.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/contacte.html.twig' : 'accueil/fr/contacte.html.twig');
     }
 
     #[Route('/{_locale}/E-Portfolio', name: 'app_e_portfolio', defaults: ['_locale' => 'fr'], requirements: ['_locale' => 'fr|en'])]
     public function portfolio(string $_locale = 'fr'): Response
     {
-        return $this->render($_locale === 'en' ? 'accueil/e-portfolio-en.html.twig' : 'accueil/e-portfolio.html.twig');
+        return $this->render($_locale === 'en' ? 'accueil/en/e-portfolio.html.twig' : 'accueil/fr/e-portfolio.html.twig');
     }
 }
